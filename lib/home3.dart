5af42bcf7b7cf5c8c3d5010234b18019f361f2c2
Nicholas Nasour <nicknasour@gmail.com>
2023-06-08 00:59:52 -0700

    lat and long no longer reset to 0

--- lib/home3.dart
+++ lib/home3.dart
@@ -11,20 +11,21 @@ class Home1 extends StatefulWidget {
   _HomeState1 createState() => _HomeState1();
 }
 
-class _HomeState1 extends State<Home1> {
-  String lat = "0";
-  String long = "0";
+// If creating lat and long as outside variables can be an issue
+// Use sharedprefences to keep the data stored
 
-  Future<Position> getCurrentLocation() async {
+String lat = "0";
+String long = "0";
+
+class _HomeState1 extends State<Home1> {
+  Future<void> getCurrentLocation() async {
     bool serviceEnabled;
     LocationPermission permission;
 
     // Test if location services are enabled.
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
+      // Location services are not enabled, don't continue accessing the position and request users of the App to enable the location services.
       return Future.error('Location services are disabled.');
     }
 
@@ -32,24 +33,22 @@ class _HomeState1 extends State<Home1> {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
+        // Permissions are denied, next time you could try requesting permissions again.
         return Future.error('Location permissions are denied');
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
       // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
+      return Future.error('Location permissions are permanently denied.');
     }
 
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition();
+    // When we reach here, permissions are granted, and we can continue accessing the position of the device.
+    Position position = await Geolocator.getCurrentPosition();
+    setState(() {
+      lat = position.latitude.toString();
+      long = position.longitude.toString();
+    });
   }
 
   void liveLocation() async {
@@ -64,7 +63,7 @@ class _HomeState1 extends State<Home1> {
       long = position.longitude.toString();
 
       CameraPosition newPosition = CameraPosition(
-        target: LatLng(position.latitude, position.longitude),
+        target: LatLng(double.parse(lat), double.parse(long)),
         zoom: 16,
       );
 
@@ -174,142 +173,3 @@ class _HomeState1 extends State<Home1> {
     );
   }
 }
-
-
-/*
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  final Completer<GoogleMapController> _controller = Completer();
-
-  static const LatLng sourceLocation = LatLng(37.4221, -122.0841);
-  static const LatLng destination = LatLng(37.4116, -122.0713);
-
-  List<LatLng> polylineCoordinates = [];
-
-  LocationData? currentLocation;
-
-  void getCurrentLocation() async {
-    Location location = Location();
-
-    location.getLocation().then(
-      (location) {
-        currentLocation = location;
-      },
-    );
-
-    if (currentLocation != null) {
-      currentLocation = currentLocation;
-    }
-
-    GoogleMapController googleMapController = await _controller.future;
-
-    location.onLocationChanged.listen(
-      (newLoc) {
-        currentLocation = newLoc;
-
-        googleMapController.animateCamera(
-          CameraUpdate.newCameraPosition(
-            CameraPosition(
-              zoom: 13.5,
-              target: LatLng(newLoc.latitude!, newLoc.longitude!),
-            ),
-          ),
-        );
-
-        setState(() {});
-      },
-    );
-  }
-
-  void getPolyPoints() async {
-    PolylinePoints polylinePoints = PolylinePoints();
-
-    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
-      google_api_key,
-      PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
-      PointLatLng(destination.latitude, destination.longitude),
-    );
-
-    if (result.points.isNotEmpty) {
-      result.points.forEach(
-        (PointLatLng point) => polylineCoordinates.add(
-          LatLng(point.latitude, point.longitude),
-        ),
-      );
-      setState(() {});
-    }
-  }
-
-  @override
-  void initState() {
-    getCurrentLocation();
-    getPolyPoints();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-    final width = screenSize.width * 0.98;
-    final height = screenSize.height * .25;
-
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: width,
-              height: height,
-              child: currentLocation == null
-                  ? const Center(child: Text("Loading"))
-                  : GoogleMap(
-                      initialCameraPosition: CameraPosition(
-                          target: LatLng(currentLocation!.latitude!,
-                              currentLocation!.longitude!),
-                          zoom: 14.5),
-                      polylines: {
-                        Polyline(
-                          polylineId: PolylineId("route"),
-                          points: polylineCoordinates,
-                          color: Colors.lightBlue,
-                          width: 5,
-                        ),
-                      },
-                      markers: {
-                        const Marker(
-                          markerId: MarkerId("source"),
-                          position: sourceLocation,
-                        ),
-                        Marker(
-                          markerId: const MarkerId("current"),
-                          position: LatLng(currentLocation!.latitude!,
-                              currentLocation!.longitude!),
-                        ),
-                        const Marker(
-                          markerId: MarkerId("destination"),
-                          position: destination,
-                        ),
-                      },
-                      onMapCreated: (mapController) {
-                        _controller.complete(mapController);
-                      },
-                    ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Latitude: ${currentLocation?.latitude}\nLongitude: ${currentLocation?.longitude}',
-              style: TextStyle(fontSize: 16),
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-*/
