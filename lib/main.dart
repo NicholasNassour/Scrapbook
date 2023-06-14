bd2bee78e52e7cf1e467b857694e46d48632de6d
Nicholas Nasour <nicknasour@gmail.com>
2023-06-13 20:02:42 -0700

    Added feature to keep user signed in after closing app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_application_1/src/navpages/main_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_application_1/home3.dart';
+import 'package:flutter_application_1/src/navpages/main_page.dart';
 import 'login.dart';
+import 'dart:async';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,18 +18,66 @@ Future<void> main() async {
         appId: "1:779783460629:web:ea702cf226f735d5d43b1c",
         measurementId: "G-677L8N9TZ6"),
   );
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late StreamSubscription<User?> user;
+  void initState() {
+    super.initState();
+    user = FirebaseAuth.instance.authStateChanges().listen((user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+        print(user.email);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    user.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter auth Demo',
-      home: Login(),
+    return MaterialApp(
+      /// check if user is signed (Open Chat page) if user is not signed in (open Login page)
+      initialRoute:
+          FirebaseAuth.instance.currentUser == null ? Login.id : MainPage.id,
+
+      ///key value pair
+      routes: {
+        MainPage.id: (context) => MainPage(),
+        Login.id: (context) => Login(),
+      },
+      home: MainPage(),
     );
   }
 }
+
+
+
+
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       title: 'Flutter auth Demo',
+//       home: Login(),
+//     );
+//   }
+// }
